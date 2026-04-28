d257efc34d55b623a3d5d6baab0e9612994c72ab
Kenzkyi <ekeneokoye93@gmail.com>
2026-04-28 13:17:24 +0100

    fix: simplify email authentication by removing OAuth2 configuration

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,11 +5,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE,
   auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
+    user: process.env.EMAIL_USER, // Your email
+    pass: process.env.EMAIL_PASS, // The code from Google
   },
 });
 exports.sendBirthdayEmail = async function (recipientEmail, name) {
